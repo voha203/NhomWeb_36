0edab1fa3ec4e6c08fc2031863b20dc5081f4d18
Võ Phượng Tường <22130316@st.hcmuaf.edu.vn>
2024-11-17 12:18:42 +0700

    làm hiệu ứng cho hình ảnh

--- main.js
+++ main.js
@@ -2,3 +2,16 @@
 function changeImage(imageSrc) {
     document.getElementById('displayed-image').src = imageSrc;
 }
+
+// Hàm thay đổi hình ảnh chính
+function changeImage(imageSrc) {
+    const mainImage = document.querySelector('.main_image img'); // Chọn hình ảnh chính
+    mainImage.src = imageSrc; // Thay đổi src của hình ảnh chính
+}
+
+// Gán sự kiện onclick cho các hình ảnh nhỏ
+document.querySelectorAll('.thumbnail').forEach((thumbnail) => {
+    thumbnail.addEventListener('click', function () {
+        changeImage(this.src); // Thay đổi hình ảnh chính dựa trên hình ảnh nhỏ được bấm
+    });
+});
