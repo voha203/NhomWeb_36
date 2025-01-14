2414458da0802cb1fafb66f071ae749d6f114635
voha203 <haduyphamvo@gmail.com>
2025-01-14 14:32:34 +0700

    tạo model

---
+++ src/main/java/model/Login.java
@@ -0,0 +1,49 @@
+package model;
+
+public class Login {
+    private int id;
+    private String username;
+    private String email;
+    private String password;
+
+    public Login() {}
+
+    public Login(int id, String username, String email, String password) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
