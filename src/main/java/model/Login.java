79de2111e8aa363e50d8bd705c4e2745055b45a9
voha203 <haduyphamvo@gmail.com>
2025-01-14 14:49:10 +0700

    model LOgin

--- src/main/java/model/Login.java
+++ src/main/java/model/Login.java
@@ -6,44 +6,13 @@ public class Login {
     private String email;
     private String password;
 
-    public Login() {}
+    public Login() {
+    }
 
     public Login(int id, String username, String email, String password) {
         this.id = id;
         this.username = username;
         this.email = email;
-        this.password = password;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
         this.password = password;
     }
 }
